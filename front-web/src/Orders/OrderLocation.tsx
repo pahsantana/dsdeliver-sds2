cee7d1b5220fd822972997840b9d6f10b4601d8b
Paloma Santana <santanagilberto@uol.com.br>
2021-01-09 23:36:57 -0300

    Finish front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { fetchLocalMapBox } from './api';
 import AsyncSelect from 'react-select/async';
-import { OrderLocationdata } from './types';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
     lat:-23.5507198, 
@@ -19,7 +19,7 @@ type Place = {
 }
 
 type Props = {
-    onChangeLocation:(location: OrderLocationdata)=>void;
+    onChangeLocation:(location: OrderLocationData)=>void;
 }
 
 function OrderLocation ({onChangeLocation}:Props){
